69eec3093498c5a0c8a3075ff606f91475c4c8d3
wangxi <112324550+luckywangxi@users.noreply.github.com>
2023-03-26 11:15:10 +0800

    修改首页页面与chatgpt官网一致，并添加翻译

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -491,12 +491,63 @@ onUnmounted(() => {
           class="w-full max-w-screen-xl m-auto dark:bg-[#101014]"
           :class="[isMobile ? 'p-2' : 'p-4']"
         >
-          <template v-if="!dataSources.length">
-            <div class="flex items-center justify-center mt-4 text-center text-neutral-300">
-              <SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-              <span>Aha~</span>
-            </div>
-          </template>
+        <template v-if="!dataSources.length">
+          <div class="flex flex-col items-center text-sm">
+            <div class="text-gray-800 w-full md:max-w-2xl lg:max-w-3xl md:h-full md:flex md:flex-col px-6 dark:text-gray-100">
+              <h1 class="text-4xl font-semibold text-center mt-6 sm:mt-[20vh] ml-auto mr-auto mb-10 sm:mb-16 flex gap-2 items-center justify-center">ChatGPT</h1>
+              <div class="md:flex items-start text-center gap-3.5">
+                <div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                  <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                    <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                      <circle cx="12" cy="12" r="5"></circle>
+                      <line x1="12" y1="1" x2="12" y2="3"></line>
+                      <line x1="12" y1="21" x2="12" y2="23"></line>
+                      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line>
+                      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line>
+                      <line x1="1" y1="12" x2="3" y2="12"></line>
+                      <line x1="21" y1="12" x2="23" y2="12"></line>
+                      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line>
+                      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
+                    </svg>
+                    {{ $t('home.Examples') }}
+                  </h2>
+                  <ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
+                    <button class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900">{{ $t('home.Examples1') }}</button>
+                    <button class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900">{{ $t('home.Examples2') }}</button>
+                    <button class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md hover:bg-gray-200 dark:hover:bg-gray-900">{{ $t('home.Examples3') }}</button>
+                  </ul>
+                </div>
+                <div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                  <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="h-6 w-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z"></path>
+                    </svg>
+                    {{ $t('home.Capabilities') }}
+                  </h2>
+                  <ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
+                    <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">{{ $t('home.Capabilities1') }}</li>
+                    <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">{{ $t('home.Capabilities2') }}</li>
+                    <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">{{ $t('home.Capabilities3') }}</li>
+                  </ul>
+                </div>
+                <div class="flex flex-col mb-8 md:mb-auto gap-3.5 flex-1">
+                  <h2 class="flex gap-3 items-center m-auto text-lg font-normal md:flex-col md:gap-2">
+                    <svg stroke="currentColor" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+                      <line x1="12" y1="9" x2="12" y2="13"></line>
+                      <line x1="12" y1="17" x2="12.01" y2="17"></line>
+                    </svg>
+                    {{ $t('home.Limitations') }}
+                  </h2>
+                  <ul class="flex flex-col gap-3.5 w-full sm:max-w-md m-auto">
+                    <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">{{ $t('home.Limitations1') }}</li>
+                    <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">{{ $t('home.Limitations2') }}</li>
+                    <li class="w-full bg-gray-50 dark:bg-white/5 p-3 rounded-md">{{ $t('home.Limitations3') }}</li>
+                  </ul>
+                </div>
+              </div>
+           </div>
+        </template>
           <template v-else>
             <div>
               <Message
