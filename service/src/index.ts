298a4ae8838357fd830b7e94ff2e5615e9429c87
jmal <zhushilun084@gmail.com>
2024-01-11 11:04:35 +0800

    fix: chat record

--- service/src/index.ts
+++ service/src/index.ts
@@ -248,10 +248,7 @@ router.get('/chat-history', auth, async (req, res) => {
       const config = await getCacheConfig()
       if (config.siteConfig.loginEnabled) {
         try {
-          const token = req.header('Authorization').replace('Bearer ', '')
-          const info = jwt.verify(token, config.siteConfig.loginSalt.trim())
-          req.headers.userId = info.userId
-          const user = await getUserById(info.userId)
+          const user = await getUserById(userId)
           if (user == null || user.status !== Status.Normal || !user.roles.includes(UserRole.Admin)) {
             res.send({ status: 'Fail', message: '无权限 | No permission.', data: null })
             return
