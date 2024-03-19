46e7f137b55bc0334dc0439650a89ecb446f6478
InternetBugs <nightjun233@gmail.com>
2024-03-19 21:49:01 +0800

    修改了image图片处理逻辑

--- service/src/utils/image.ts
+++ service/src/utils/image.ts
@@ -12,32 +12,11 @@ fs.mkdir('uploads').then(() => {
 })
 
 export async function convertImageUrl(uploadFileKey: string): Promise<string> {
-  try {
-    const imageData = await fs.readFile(`uploads/${uploadFileKey}`)
-    const imageType = await fileType.fileTypeFromBuffer(imageData)
-
-    if (!imageType) {
-      throw new Error('不支持的图片格式')
-    }
-
-    let mimeType: string
-    switch (imageType.mime) {
-      case 'image/png':
-      case 'image/jpeg':
-      case 'image/webp':
-      case 'image/gif':
-        mimeType = imageType.mime
-        break
-      default:
-        throw new Error('不支持的图片格式')
-        break
-    }
-
-    // 将图片数据转换为 Base64 编码的字符串，并返回带有正确 MIME 类型的字符串
-    const base64Image = imageData.toString('base64')
-    return `data:${mimeType};base64,${base64Image}`
-  } catch (error) {
-    console.error('转换图片 URL 出错:', error)
-    return null
-  }
+  const imageData = await fs.readFile(`uploads/${uploadFileKey}`)
+  //判断文件格式
+  const imageType = await fileType.fileTypeFromBuffer(imageData)
+  const mimeType = imageType.mime;
+  // 将图片数据转换为 Base64 编码的字符串
+  const base64Image = imageData.toString('base64')
+  return `data:${mimeType};base64,${base64Image}`
 }
