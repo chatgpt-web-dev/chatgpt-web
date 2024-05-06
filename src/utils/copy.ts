ade9f3bda3bb48e6c89c594ac3d2e12b0d6f5eb6
Bob Du <i@bobdu.cc>
2024-05-06 22:17:52 +0800

    fix: remove trailling newline when copy text
    Signed-off-by: Bob Du <i@bobdu.cc>

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -1,18 +1,11 @@
+document.addEventListener('copy', (e) => {
+  const selectedText = window.getSelection()?.toString() ?? ''
+  const cleanedText = selectedText?.replace(/\n+$/, '\n')
+  e.clipboardData?.setData('text/plain', cleanedText)
+
+  e.preventDefault()
+})
+
 export function copyToClip(text: string) {
-  return new Promise((resolve, reject) => {
-    try {
-      const input: HTMLTextAreaElement = document.createElement('textarea')
-      input.setAttribute('readonly', 'readonly')
-      input.value = text
-      document.body.appendChild(input)
-      input.select()
-      if (document.execCommand('copy'))
-        document.execCommand('copy')
-      document.body.removeChild(input)
-      resolve(text)
-    }
-    catch (error) {
-      reject(error)
-    }
-  })
+  return navigator.clipboard.writeText(text)
 }
