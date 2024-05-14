9ed2e25d3becc3f8f66778ecd08b678dd118f14e
San <zhujunsan@gmail.com>
2024-05-14 10:20:46 +0800

    fix isVisionModel assert, add gpt-4o (#533)

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -56,7 +56,7 @@ const currentChatModel = computed(() => nowSelectChatModel.value ?? currentChatH
 
 const currentNavIndexRef = ref<number>(-1)
 
-const isVisionModel = computed(() => currentChatModel.value && (currentChatModel.value?.includes('vision') || ['gpt-4-turbo', 'gpt-4-turbo-2024-04-09'].includes(currentChatModel.value)))
+const isVisionModel = computed(() => currentChatModel.value && (currentChatModel.value?.includes('vision') || ['gpt-4-turbo', 'gpt-4-turbo-2024-04-09'].includes(currentChatModel.value) || currentChatModel.value?.includes('gpt-4o')))
 
 let loadingms: MessageReactive
 let allmsg: MessageReactive
