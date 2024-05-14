c01895afb15bb755dd585536c238e3004967a15e
Bob Du <i@bobdu.cc>
2024-05-14 13:54:11 +0800

    fix: copy error in insecure page (#535)
    Signed-off-by: Bob Du <i@bobdu.cc>

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -6,6 +6,19 @@ document.addEventListener('copy', (e) => {
   e.preventDefault()
 })
 
-export function copyToClip(text: string) {
-  return navigator.clipboard.writeText(text)
+export async function copyToClip(text: string) {
+  // https://stackoverflow.com/questions/51805395/navigator-clipboard-is-undefined
+  if (navigator.clipboard && window.isSecureContext) {
+    await navigator.clipboard.writeText(text)
+  }
+  else {
+    const input: HTMLTextAreaElement = document.createElement('textarea')
+    input.setAttribute('readonly', 'readonly')
+    input.value = text
+    document.body.appendChild(input)
+    input.select()
+    if (document.execCommand('copy'))
+      document.execCommand('copy')
+    document.body.removeChild(input)
+  }
 }
