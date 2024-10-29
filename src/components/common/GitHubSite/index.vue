23b2ee18cf2085a5d791739afc2b9012b7cbe14f
wudingjian <125175432+wudingjian@users.noreply.github.com>
2024-10-30 01:34:18 +0800

    Update index.vue
    Signed-off-by: wudingjian <125175432+wudingjian@users.noreply.github.com>

--- src/components/common/GitHubSite/index.vue
+++ src/components/common/GitHubSite/index.vue
@@ -5,7 +5,7 @@ const releaseVersion = import.meta.env.VITE_RELEASE_VERSION || 'v0.0.0'
 
 <template>
   <div class="text-center text-xs">
-    <img src="https://img.188181.xyz/i/2024/05/05/1dvsz-0.webp" alt="造价e星球" style="width: 20px; height: 20px; display: inline-block;"> <a href="https://www.zje.cc" class="text-blue-500" target="_blank" style="display: inline-block;">造价e星球</a>
+    <img src="https://img.zje.cc/i/2024/05/05/1dvsz-0.webp" alt="造价e星球" style="width: 20px; height: 20px; display: inline-block;"> <a href="https://www.zje.cc" class="text-blue-500" target="_blank" style="display: inline-block;">造价e星球</a>
     <span v-text="`${releaseVersion}-${gitCommitSha}`" />
   </div>
 </template>
