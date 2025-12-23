41f7ad105e210c7009b9fc2f048f4844add57a59
xiaokai.wang <xiaokai.wang@dragonplus.com>
2025-12-23 18:21:53 +0800

    fix: lint

--- service/src/utils/s3.ts
+++ service/src/utils/s3.ts
@@ -1,3 +1,4 @@
+import type { Buffer } from 'node:buffer'
 import type { SiteConfig } from '../storage/model'
 import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
