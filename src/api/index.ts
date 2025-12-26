4bc0a2fd8f502ad6fa46cfbad6b21b5b65d8f63d
xiaokai.wang <xiaokai.wang@dragonplus.com>
2025-12-26 18:46:42 +0800

    fix: type check

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,3 @@
-import type { ImageGenerationTool } from '../../service/src/types'
 import type { AnnounceConfig, AuditConfig, ConfigState, GiftCard, KeyConfig, MailConfig, SearchConfig, SiteConfig, Status, UserInfo, UserPassword, UserPrompt } from '@/components/common/Setting/model'
 import type { SettingsState } from '@/store/modules/user/helper'
 import { useUserStore } from '@/store'
@@ -40,7 +39,7 @@ export function fetchChatAPIProcessSSE(
     prompt: string
     uploadFileKeys?: string[]
     options?: { conversationId?: string, parentMessageId?: string }
-    tools?: Array<ImageGenerationTool>
+    tools?: Array<Chat.ImageGenerationTool>
     previousResponseId?: string
     signal?: AbortSignal
   },
